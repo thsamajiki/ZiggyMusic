00a7b2a8b2ddaa401ca1e5135ebcaf805589ff2d
thsamajiki <ant1102@naver.com>
2023-05-14 14:47:19 +0900

    설명 주석 추가

--- app/src/main/java/com/hero/ziggymusic/ext/BackgroundExt.kt
+++ app/src/main/java/com/hero/ziggymusic/ext/BackgroundExt.kt
@@ -6,8 +6,9 @@ import androidx.databinding.BindingAdapter
 
 @BindingAdapter("playStateBackground")
 fun ViewGroup.setPlayStateBackground(isPlaying: Boolean) {
+    // 재생 중에 따라
     if (isPlaying) {
-        setBackgroundColor(Color.GRAY)
+        setBackgroundColor(Color.GRAY) // 재생 중이면 배경 색을 회색
     } else {
         setBackgroundColor(Color.TRANSPARENT)
     }
