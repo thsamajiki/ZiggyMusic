52340b2ea5b54a0e9132950708154320d6679760
thsamajiki <ant1102@naver.com>
2023-05-14 14:55:14 +0900

    음원 데이터에 앨범명 추가

--- app/src/main/java/com/hero/ziggymusic/database/local/MusicLocalDataSourceImpl.kt
+++ app/src/main/java/com/hero/ziggymusic/database/local/MusicLocalDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.hero.ziggymusic.database.local
 
 import android.app.Application
 import android.provider.MediaStore
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.hero.ziggymusic.database.music.dao.MusicFileDao
 import com.hero.ziggymusic.database.music.dao.PlaylistMusicDao
@@ -17,14 +18,17 @@ class MusicLocalDataSourceImpl @Inject constructor(
     override suspend fun loadMusics() {
         val musicListUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
 
+        // 2. 가져올 데이터 컬럼을 정의
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
             MediaStore.Audio.Media.TITLE,
             MediaStore.Audio.Media.ARTIST,
             MediaStore.Audio.Media.ALBUM_ID,
+            MediaStore.Audio.Media.ALBUM,
             MediaStore.Audio.Media.DURATION
         )
 
+        // 3. 콘텐츠 리졸버에 해당 데이터 요청 (음원 목록에 있는 0번째 줄을 가리킴)
         val cursor = application.contentResolver.query(
             musicListUri,
             projection,
@@ -33,6 +37,7 @@ class MusicLocalDataSourceImpl @Inject constructor(
             null
         )
 
+        // 4. 커서로 전달된 데이터를 꺼내서 저장
         val musicList = mutableListOf<MusicModel>()
 
         while (cursor?.moveToNext() == true) {
@@ -40,9 +45,10 @@ class MusicLocalDataSourceImpl @Inject constructor(
             val title = cursor.getString(1)
             val artist = cursor.getString(2)
             val albumId = cursor.getString(3)
-            val duration = cursor.getLong(4)
+            val albumTitle = cursor.getString(4) //  (if (cursor.getString(4) == null) "Unknown Album" else cursor.getString(4))
+            val duration = cursor.getLong(5)
 
-            val music = MusicModel(id, title, artist, albumId, duration)
+            val music = MusicModel(id, title, artist, albumId, albumTitle, duration)
             musicList.add(music)
         }
 
