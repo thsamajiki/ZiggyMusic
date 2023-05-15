7bd76de51c59886d118793e84d4cb55a35d2bf76
thsamajiki <ant1102@naver.com>
2023-05-15 16:17:23 +0900

    백그라운드 서비스 적용

--- app/src/main/java/com/hero/ziggymusic/view/main/MainViewModel.kt
+++ app/src/main/java/com/hero/ziggymusic/view/main/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.hero.ziggymusic.view.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.hero.ziggymusic.database.music.entity.MusicModel
 import com.hero.ziggymusic.database.music.repository.MusicRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,6 +11,6 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val musicRepository: MusicRepository
 ): ViewModel() {
-    
-    
+
+    val musicList: LiveData<List<MusicModel>> = musicRepository.getAllMusic()
 }
