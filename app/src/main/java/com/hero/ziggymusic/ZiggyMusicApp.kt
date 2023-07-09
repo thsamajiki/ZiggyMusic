2ced72c8230d9150df83446d45a99a20553882a8
thsamajiki <ant1102@naver.com>
2023-07-10 01:16:50 +0900

    ExoPlayer를 싱글톤으로 선언

--- app/src/main/java/com/hero/ziggymusic/ZiggyMusicApp.kt
+++ app/src/main/java/com/hero/ziggymusic/ZiggyMusicApp.kt
@@ -1,11 +1,15 @@
 package com.hero.ziggymusic
 
 import android.app.Application
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import com.google.android.exoplayer2.ExoPlayer
 import dagger.hilt.android.HiltAndroidApp
 import kotlin.text.Typography.dagger
 
 @HiltAndroidApp
 class ZiggyMusicApp : Application() {
+
+    val exoPlayer by lazy { ExoPlayer.Builder(this).build() }
 
     companion object {
         lateinit var instance : ZiggyMusicApp
