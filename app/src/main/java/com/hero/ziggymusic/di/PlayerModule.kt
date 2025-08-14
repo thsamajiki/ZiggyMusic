c1a809009da2e50c5cc6b77dde13ff8da38b6683
thsamajiki <ant1102@naver.com>
2025-08-14 13:35:37 +0900

    ExoPlayer 인스턴스 주입을 위한 의존성 주입 모듈 생성

---
+++ app/src/main/java/com/hero/ziggymusic/di/PlayerModule.kt
@@ -0,0 +1,19 @@
+package com.hero.ziggymusic.di
+
+import android.content.Context
+import androidx.media3.exoplayer.ExoPlayer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PlayerModule {
+    @Provides
+    @Singleton
+    fun provideExoPlayer(@ApplicationContext context: Context): ExoPlayer =
+        ExoPlayer.Builder(context).build()
+}
