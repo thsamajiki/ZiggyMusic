8e3e547599492c81b4f886ad472c1c9584e2ef9b
thsamajiki <ant1102@naver.com>
2025-08-24 00:04:59 +0900

    View.OnClickListener 인터페이스 제거

--- app/src/main/java/com/hero/ziggymusic/view/main/MainActivity.kt
+++ app/src/main/java/com/hero/ziggymusic/view/main/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.OptIn
