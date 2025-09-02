c6f9adb12ea96a4f1acd6b8eb294d3fb5d0faaf6
thsamajiki <ant1102@naver.com>
2025-09-02 09:12:31 +0900

    compileSdk, targetSdk를 35로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.hero.ziggymusic"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hero.ziggymusic"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
