0ac979d1a079bf0258fa49652e1ef29201bb90ed
thsamajiki <ant1102@naver.com>
2026-06-23 16:43:38 +0900

    [ Refactor ] `MusicFileDao` 내 테이블 이름 변경 (`music_table` -> `music`)
    - `getAllFiles`, `getMusicCount`, `getMusicFileFromKey`, `clearAll`, `deleteFilesExcept` 등 DAO 내 모든 SQL 쿼리의 대상 테이블명을 `music_table`에서 `music`으로 수정

--- app/src/main/java/com/hero/ziggymusic/database/music/dao/MusicFileDao.kt
+++ app/src/main/java/com/hero/ziggymusic/database/music/dao/MusicFileDao.kt
@@ -6,13 +6,13 @@ import com.hero.ziggymusic.database.music.entity.MusicModel
 
 @Dao
 interface MusicFileDao {
-    @Query("SELECT * FROM music_table ORDER BY title ASC")
+    @Query("SELECT * FROM music ORDER BY title ASC")
     fun getAllFiles() : LiveData<List<MusicModel>>
 
-    @Query("SELECT COUNT(*) FROM music_table")
+    @Query("SELECT COUNT(*) FROM music")
     suspend fun getMusicCount(): Int
 
-    @Query("SELECT * FROM music_table WHERE id = :key limit 1")
+    @Query("SELECT * FROM music WHERE id = :key limit 1")
     suspend fun getMusicFileFromKey(key: String?): MusicModel?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -21,12 +21,12 @@ interface MusicFileDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(musicList: List<MusicModel>)
 
-    @Query("DELETE FROM music_table")
+    @Query("DELETE FROM music")
     suspend fun clearAll()
 
     @Delete
     fun deleteMusic(musicModel: MusicModel)
 
-    @Query("DELETE FROM music_table WHERE id NOT IN (:musicIdList)")
+    @Query("DELETE FROM music WHERE id NOT IN (:musicIdList)")
     suspend fun deleteFilesExcept(musicIdList: List<String>)
 }
