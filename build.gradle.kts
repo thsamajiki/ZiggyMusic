ae053619ba362d4cd5d59432e995519e11b5b272
thsamajiki <ant1102@naver.com>
2026-07-10 18:59:24 +0900

    [ Chore ] `AboutLibraries` 플러그인 추가 및 미사용 의존성 제거
    - `AboutLibraries` 플러그인 및 관련 라이브러리 의존성 추가
    - 미사용 `lottie` 라이브러리 및 버전 정보 제거
    - 미사용 `lifecycle-viewmodel-compose-android` 라이브러리 항목 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,6 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.aboutlibraries) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
